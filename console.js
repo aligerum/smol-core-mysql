0f306e052142e163a582eeaf5fd102b00f69a473
Allister Aligerum <allister@aligerum.io>
2020-06-17 22:42:46 -0400

    remove models from console

--- console.js
+++ console.js
@@ -1,12 +1,2 @@
-const fs = require('fs')
-const modelScript = require('./script/model')
-
 module.exports = (coreName, core, context) => {
-
-  // load all models
-  core.models = {}
-  if (fs.existsSync(`${process.cwd()}/core/${coreName}/model`)) {
-    for (let modelName of fs.readdirSync(`${process.cwd()}/core/${coreName}/model`).map(item => item.slice(0, -3))) core.models[modelName] = modelScript(`${coreName}/${modelName}`)
-  }
-
 }
